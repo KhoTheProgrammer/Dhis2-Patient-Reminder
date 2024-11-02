a63961705fe84c2a94a21ce437d8212b543b6796
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-02 16:39:00 +0200

    Started developing a sidebar

---
+++ src/assets/Sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SideBar.css";
+
+const Sidebar = () => {
+  return (
+    <div >
+      <div className="sidebar">
+      <p>PATIENT</p>
+      <p>ENROLL PATIENT</p>
+      <p>SENT MESSAGES</p>
+      <p>FOLLOW UP</p>
+      <p>PATIENT PROGRESS</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
