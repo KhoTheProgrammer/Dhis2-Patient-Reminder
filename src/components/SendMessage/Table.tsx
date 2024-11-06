eb7b316fc440daa375f5c105bc1be822dff6243d
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-06 12:55:47 +0200

    implemented Table component

--- src/components/SendMessage/Table.tsx
+++ src/components/SendMessage/Table.tsx
@@ -1,142 +1,148 @@
 import React from "react";
+import './Table.css'
+
 import {
   Table,
   TableHead,
   TableBody,
   TableRow,
   TableCell,
-  TableFoot,
-  TableRowHead,
-  TableCellHead
+  Button,
+  TableCellHead,
 } from "@dhis2/ui";
 
-const TableMessage = () => {
-  return (
-    <div className="table">
-      <Table>
-        <TableHead>
-          <TableRowHead>
-            <TableCellHead>First name</TableCellHead>
-            <TableCellHead>Last name</TableCellHead>
-            <TableCellHead>Incident date</TableCellHead>
-            <TableCellHead>Last updated</TableCellHead>
-            <TableCellHead>Age</TableCellHead>
-            <TableCellHead>Registering unit</TableCellHead>
-            <TableCellHead>Assigned user</TableCellHead>
-            <TableCellHead>Status</TableCellHead>
-          </TableRowHead>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Onyekachukwu</TableCell>
-            <TableCell>Kariuki</TableCell>
-            <TableCell>02/06/2007</TableCell>
-            <TableCell>05/25/1972</TableCell>
-            <TableCell>66</TableCell>
-            <TableCell>Jawi</TableCell>
-            <TableCell>Sofie Hubert</TableCell>
-            <TableCell>Incomplete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Kwasi</TableCell>
-            <TableCell>Okafor</TableCell>
-            <TableCell>08/11/2010</TableCell>
-            <TableCell>02/26/1991</TableCell>
-            <TableCell>38</TableCell>
-            <TableCell>Mokassie MCHP</TableCell>
-            <TableCell>Dashonte Clarke</TableCell>
-            <TableCell>Complete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Siyabonga</TableCell>
-            <TableCell>Abiodun</TableCell>
-            <TableCell>07/21/1981</TableCell>
-            <TableCell>02/06/2007</TableCell>
-            <TableCell>98</TableCell>
-            <TableCell>Bathurst MCHP</TableCell>
-            <TableCell>Unassigned</TableCell>
-            <TableCell>Incomplete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Chiyembekezo</TableCell>
-            <TableCell>Okeke</TableCell>
-            <TableCell>01/23/1982</TableCell>
-            <TableCell>07/15/2003</TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>Mayolla MCHP</TableCell>
-            <TableCell>Wan Gengxin</TableCell>
-            <TableCell>Incomplete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Mtendere</TableCell>
-            <TableCell>Afolayan</TableCell>
-            <TableCell>08/12/1994</TableCell>
-            <TableCell>05/12/1972</TableCell>
-            <TableCell>37</TableCell>
-            <TableCell>Gbangadu MCHP</TableCell>
-            <TableCell>Gvozden Boskovsky</TableCell>
-            <TableCell>Complete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Inyene</TableCell>
-            <TableCell>Okonkwo</TableCell>
-            <TableCell>04/01/1971</TableCell>
-            <TableCell>03/16/2000</TableCell>
-            <TableCell>70</TableCell>
-            <TableCell>Kunike Barina</TableCell>
-            <TableCell>Oscar de la Cavallería</TableCell>
-            <TableCell>Complete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Amaka</TableCell>
-            <TableCell>Pretorius</TableCell>
-            <TableCell>01/25/1996</TableCell>
-            <TableCell>09/15/1986</TableCell>
-            <TableCell>32</TableCell>
-            <TableCell>Bargbo</TableCell>
-            <TableCell>Alberto Raya</TableCell>
-            <TableCell>Incomplete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Meti</TableCell>
-            <TableCell>Abiodun</TableCell>
-            <TableCell>10/24/2010</TableCell>
-            <TableCell>07/26/1989</TableCell>
-            <TableCell>8</TableCell>
-            <TableCell>Majihun MCHP</TableCell>
-            <TableCell>Unassigned</TableCell>
-            <TableCell>Complete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Eshe</TableCell>
-            <TableCell>Okeke</TableCell>
-            <TableCell>01/31/1995</TableCell>
-            <TableCell>01/31/1995</TableCell>
-            <TableCell>63</TableCell>
-            <TableCell>Mambiama CHP</TableCell>
-            <TableCell>Shadrias Pearson</TableCell>
-            <TableCell>Incomplete</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Obi</TableCell>
-            <TableCell>Okafor</TableCell>
-            <TableCell>06/07/1990</TableCell>
-            <TableCell>01/03/2006</TableCell>
-            <TableCell>28</TableCell>
-            <TableCell>Dalakuru CHP</TableCell>
-            <TableCell>Anatoliy Shcherbatykh</TableCell>
-            <TableCell>Incomplete</TableCell>
-          </TableRow>
-        </TableBody>
-        <TableFoot>
-          <TableRow>
-            <TableCell colSpan="8">
+const data = [
+  {
+    firstName: "Onyekachukwu",
+    lastName: "Kariuki",
+    incidentDate: "02/06/2007",
+    lastUpdated: "05/25/1972",
+    age: 66,
+    registeringUnit: "Jawi",
+    assignedUser: "Sofie Hubert",
+    status: "Incomplete",
+  },
+  {
+    firstName: "Kwasi",
+    lastName: "Okafor",
+    incidentDate: "08/11/2010",
+    lastUpdated: "02/26/1991",
+    age: 38,
+    registeringUnit: "Mokassie MCHP",
+    assignedUser: "Dashonte Clarke",
+    status: "Complete",
+  },
+  {
+    firstName: "Siyabonga",
+    lastName: "Abiodun",
+    incidentDate: "07/21/1981",
+    lastUpdated: "02/06/2007",
+    age: 98,
+    registeringUnit: "Bathurst MCHP",
+    assignedUser: "Unassigned",
+    status: "Incomplete",
+  },
+  {
+    firstName: "Chiyembekezo",
+    lastName: "Okeke",
+    incidentDate: "01/23/1982",
+    lastUpdated: "07/15/2003",
+    age: 2,
+    registeringUnit: "Mayolla MCHP",
+    assignedUser: "Wan Gengxin",
+    status: "Incomplete",
+  },
+  {
+    firstName: "Mtendere",
+    lastName: "Afolayan",
+    incidentDate: "08/12/1994",
+    lastUpdated: "05/12/1972",
+    age: 37,
+    registeringUnit: "Gbangadu MCHP",
+    assignedUser: "Gvozden Boskovsky",
+    status: "Complete",
+  },
+  {
+    firstName: "Inyene",
+    lastName: "Okonkwo",
+    incidentDate: "04/01/1971",
+    lastUpdated: "03/16/2000",
+    age: 70,
+    registeringUnit: "Kunike Barina",
+    assignedUser: "Oscar de la Cavallería",
+    status: "Complete",
+  },
+  {
+    firstName: "Amaka",
+    lastName: "Pretorius",
+    incidentDate: "01/25/1996",
+    lastUpdated: "09/15/1986",
+    age: 32,
+    registeringUnit: "Bargbo",
+    assignedUser: "Alberto Raya",
+    status: "Incomplete",
+  },
+  {
+    firstName: "Meti",
+    lastName: "Abiodun",
+    incidentDate: "10/24/2010",
+    lastUpdated: "07/26/1989",
+    age: 8,
+    registeringUnit: "Majihun MCHP",
+    assignedUser: "Unassigned",
+    status: "Complete",
+  },
+  {
+    firstName: "Eshe",
+    lastName: "Okeke",
+    incidentDate: "01/31/1995",
+    lastUpdated: "01/31/1995",
+    age: 63,
+    registeringUnit: "Mambiama CHP",
+    assignedUser: "Shadrias Pearson",
+    status: "Incomplete",
+  },
+  {
+    firstName: "Obi",
+    lastName: "Okafor",
+    incidentDate: "06/07/1990",
+    lastUpdated: "01/03/2006",
+    age: 28,
+    registeringUnit: "Dalakuru CHP",
+    assignedUser: "Anatoliy Shcherbatykh",
+    status: "Incomplete",
+  },
+];
+
+const TableMessage = () => (
+  <div className="table">
+    <Table>
+      <TableHead className="tablehead">
+        <TableRow>
+          <TableCellHead>First name</TableCellHead>
+          <TableCellHead>Last name</TableCellHead>
+          <TableCellHead>Date Enrolled</TableCellHead>
+          <TableCellHead>Organisation unit</TableCellHead>
+          <TableCellHead>Health Program</TableCellHead>
+          <TableCellHead>Add Appointment</TableCellHead>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((person, index) => (
+          <TableRow key={index} className="tablerow">
+            <TableCell>{person.firstName}</TableCell>
+            <TableCell>{person.lastName}</TableCell>
+            <TableCell>{person.incidentDate}</TableCell>
+            <TableCell>{person.registeringUnit}</TableCell>
+            <TableCell>{person.assignedUser}</TableCell>
+            <TableCell>
+              <Button className="button">Add</Button>
             </TableCell>
           </TableRow>
-        </TableFoot>
-      </Table>
-    </div>
-  );
-};
+        ))}
+      </TableBody>
+    </Table>
+  </div>
+);
 
 export default TableMessage;
