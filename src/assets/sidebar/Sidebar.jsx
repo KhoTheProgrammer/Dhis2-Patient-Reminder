96b0b89d5d66d718c09974e52f1688edf2e76f8e
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-06 13:30:09 +0200

    resized icons

--- src/assets/sidebar/Sidebar.jsx
+++ src/assets/sidebar/Sidebar.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as PatientIcon } from '../../icons/patient.svg';
-import { ReactComponent as RegisterIcon } from '../../icons/register.svg';
-import { ReactComponent as EnrollIcon } from '../../icons/enroll.svg';
-import { ReactComponent as Email } from '../../icons/email.svg';
-import { ReactComponent as FollowUpIcon } from '../../icons/followup.svg';
-import { ReactComponent as ProgressIcon } from '../../icons/progress.svg';
+import PatientIcon from '../../icons/patient.png';
+import RegisterIcon from '../../icons/register.png';
+import EnrollIcon from '../../icons/enroll.png';
+import EmailIcon from '../../icons/email.png';
+import FollowUpIcon from '../../icons/followup.png';
+import ProgressIcon from '../../icons/progress.png';
 import './SideBar.css';
 
 const Sidebar = () => (
     <div className="sidebar">
         <nav>
             <NavLink to="/patients" activeClassName="active">
-                <PatientIcon /> Patient
+                <img src={PatientIcon} alt="Patient" className="icon" /> Patient
             </NavLink>
             <NavLink to="/register" activeClassName="active">
-                <RegisterIcon /> Register Patient
+                <img src={RegisterIcon} alt="Register Patient" className="icon" /> Register Patient
             </NavLink>
             <NavLink to="/enroll" activeClassName="active">
-                <EnrollIcon /> Enroll Patient
+                <img src={EnrollIcon} alt="Enroll Patient" className="icon" /> Enroll Patient
             </NavLink>
             <NavLink to="/messages" activeClassName="active">
-                <Email /> Sent Messages
+                <img src={EmailIcon} alt="Sent Messages" className="icon" /> Sent Messages
             </NavLink>
             <NavLink to="/follow-up" activeClassName="active">
-                <FollowUpIcon /> Follow Up
+                <img src={FollowUpIcon} alt="Follow Up" className="icon" /> Follow Up
             </NavLink>
             <NavLink to="/progress" activeClassName="active">
-                <ProgressIcon /> Patient's Progress
+                <img src={ProgressIcon} alt="Patient's Progress" className="icon" /> Patient's Progress
             </NavLink>
         </nav>
     </div>
