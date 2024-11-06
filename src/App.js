f9d7f9e220418985b2cd915992b6562ffd187f6f
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-06 10:31:38 +0200

    added side bar in app

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Register from './assets/Register';
 const App = () => (
   <div>
       <BrowserRouter>
+      <Sidebar/>
         <Routes>
           <Route exact path='/' Component={HomePage} />
           <Route path="/patients" component={Patients} />
