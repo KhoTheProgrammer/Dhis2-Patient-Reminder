aa50b873e8405db68271e69dd0b5c4779b4e2c0a
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-06 14:09:48 +0200

    rerouting the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './assets/sidebar/Sidebar';
 import Patients from './assets/Patients';
 import HomePage from './assets/HomePage';
@@ -7,25 +7,26 @@ import PatientEnrollment from './components/enrollPatient/PatientEnrollment';
 import Messages from './assets/Messages';
 import FollowUp from './assets/FollowUp';
 import Progress from './assets/Progress';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import Register from './assets/Register';
-
+import './App.css';
 
 const App = () => (
-  <div>
-      <BrowserRouter>
-      <Sidebar/>
+  <Router>
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
         <Routes>
-          <Route exact path='/' Component={HomePage} />
-          <Route path="/patients" component={Patients} />
-          <Route path='/register' Component={Register} />
-          <Route path="/enroll" component={PatientEnrollment} />
-          <Route path="/messages" component={Messages} />
-          <Route path="/follow-up" component={FollowUp} />
-          <Route path="/progress" component={Progress} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/enroll" element={<PatientEnrollment />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/follow-up" element={<FollowUp />} />
+          <Route path="/progress" element={<Progress />} />
         </Routes>
-      </BrowserRouter>
-  </div>
+      </div>
+    </div>
+  </Router>
 );
 
 export default App;
