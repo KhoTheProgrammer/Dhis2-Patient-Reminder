c2425d3cb6d48c3fda82bad1db3ae84716448c36
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-08 17:09:39 +0200

    home page adjusted

---
+++ src/assets/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className='home'>
+      <h1>DHIS2</h1>
+      <h2>Patient Reminder</h2>
+      <h3>Application</h3>
+    </div>
+  );
+}
+
+export default HomePage;
