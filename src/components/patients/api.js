089d3ebc5cfa122b392e0a1cc6328c68d2145d57
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-08 16:23:07 +0200

    Included trackedEntityInstance id in the fields parameters

--- src/components/patients/api.js
+++ src/components/patients/api.js
@@ -1,18 +1,18 @@
 import { useDataQuery } from "@dhis2/app-runtime";
 
 const trackedEntityType = "nEenWmSyUEp";
-const orgUnit = "DiszpKrYNg8";
+const orgUnit = "DFyu9VGpodC";
 
 
 export const patientsQuery = {
-    trackedEntityInstances: {
-        resource: "trackedEntityInstances.json",
-        params: {
-            ou: orgUnit,
-            trackedEntityType: trackedEntityType,
-            fields:[
-                'attributes[displayName, value, created]'
-            ]
-        }
-    }
-}
+  trackedEntityInstances: {
+    resource: "trackedEntityInstances.json",
+    params: {
+      ou: orgUnit,
+      trackedEntityType: trackedEntityType,
+      fields: [
+        "attributes[displayName, value, created], trackedEntityInstance",
+      ],
+    },
+  },
+};
