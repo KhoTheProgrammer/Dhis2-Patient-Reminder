f24bc121cbfa942b7b24b3cfd0434ae680f70eb1
Justice Khaira <bsc-inf-28-22@unima.ac.mw>
2024-11-08 18:23:43 +0200

    Sent Message bar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import { useDataQuery } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
-import React from 'react'
-import classes from './App.module.css'
+import React from 'react';
+import { useDataQuery } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n';
+import HeaderBar from './components/HeaderBar'; // Importing HeaderBar component
+//import MessageTable from './components/MessageTable'; // Importing MessageTable component
+import './App.css'; // Importing global styles
 
 const query = {
     me: {
         resource: 'me',
     },
-}
+};
 
-const MyApp = () => {
-    const { error, loading, data } = useDataQuery(query)
+const App = () => {
+    const { error, loading, data } = useDataQuery(query);
 
     if (error) {
-        return <span>{i18n.t('ERROR')}</span>
+        return <span>{i18n.t('ERROR')}</span>;
     }
 
     if (loading) {
-        return <span>{i18n.t('Loading...')}</span>
+        return <span>{i18n.t('Loading...')}</span>;
     }
 
     return (
-        <div className={classes.container}>
-            <h1 className={classes.welcome}>{i18n.t('Hello {{name}}', { name: data.me.name })}</h1>
-            <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+        <div className="container">
+            <HeaderBar />
+            <h1 className="welcome">
+                {i18n.t(' ', {  })}
+            </h1>
+            <h3>{i18n.t('')}</h3>
+          
         </div>
-    )
-}
+    );
+};
 
-export default MyApp
+export default App;
---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css'; // Importing global styles
+
+const HeaderBar = () => {
+    return <div className="header-bar">Sent Messages</div>;
+};
+
+export default HeaderBar;
