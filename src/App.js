b881b0406aa6cda4f46165107a514cfeeb6496cf
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-11 18:03:39 +0200

    edited homepage again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './assets/sidebar/Sidebar';
 import NoPatientFound from './assets/NoPatientFound/NoPatientFound'
-import HomePage from './assets/HomePage';
 import PatientEnrollment from './components/enrollPatient/PatientEnrollment';
 import Messages from './assets/Messages';
 import FollowUp from './assets/FollowUp';
@@ -12,7 +11,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import Register from './components/register/Register';
 import Patients from './components/patients/Patients';
 import MessageTable from './components/Sent message/MessageTable';
-import Homepage from './components/homepage/HomePage';
+import Homepage from './components/HomePage/HomePage';
 
 const App = () => (
   <Router>
@@ -27,7 +26,6 @@ const App = () => (
           <Route path="/messages" element={<MessageTable />} />
           <Route path="/follow-up" element={<FollowUp />} />
           <Route path="/progress" element={<Progress />} />
-          <Route exact path='/' Component={HomePage} />
           <Route path="/patients" component={NoPatientFound} />
           <Route path='/register' Component={Register} />
           <Route path="/enroll" component={PatientEnrollment} />
