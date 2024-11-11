96eb4fc85b6f353d661cf19c2167282024f8ce82
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-11 11:13:57 +0200

    added headerbar on merge

---
+++ src/components/Sent message/HeaderBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Message.css"// Importing global styles
+
+const HeaderBar = () => {
+    return <div className="header-bar">Sent Messages</div>;
+};
+
+export default HeaderBar;
