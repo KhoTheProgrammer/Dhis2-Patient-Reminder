dd5f1e5181468c3677ffbb8715bcdfc2f3706d9d
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-11 11:08:52 +0200

    merged conflicts in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './assets/sidebar/Sidebar';
-import Patients from './assets/Patients';
-import HomePage from './components/HomePage/HomePage';
+import NoPatientFound from './assets/NoPatientFound/NoPatientFound'
+import HomePage from './assets/HomePage';
 import PatientEnrollment from './components/enrollPatient/PatientEnrollment';
 import Messages from './assets/Messages';
 import FollowUp from './assets/FollowUp';
 import Progress from './assets/Progress';
-import Register from './components/register/Register';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Register from './components/register/Register';
+import Patients from './components/patients/Patients';
+import MessageTable from './components/Sent message/MessageTable';
+import Homepage from './components/homepage/HomePage';
 
 const App = () => (
   <Router>
@@ -16,13 +20,20 @@ const App = () => (
       <Sidebar />
       <div className="content">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/patients" element={<Patients />} />
           <Route path="/register" element={<Register />} />
           <Route path="/enroll" element={<PatientEnrollment />} />
-          <Route path="/messages" element={<Messages />} />
+          <Route path="/messages" element={<MessageTable />} />
           <Route path="/follow-up" element={<FollowUp />} />
           <Route path="/progress" element={<Progress />} />
+          <Route exact path='/' Component={HomePage} />
+          <Route path="/patients" component={NoPatientFound} />
+          <Route path='/register' Component={Register} />
+          <Route path="/enroll" component={PatientEnrollment} />
+          <Route path="/messages" component={Messages} />
+          <Route path="/follow-up" component={FollowUp} />
+          <Route path="/progress" component={Progress} />
         </Routes>
       </div>
     </div>
@@ -30,3 +41,4 @@ const App = () => (
 );
 
 export default App;
+
