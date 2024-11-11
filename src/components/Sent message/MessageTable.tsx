94b031c3c72b335b7717c7e31bd7305191b8de23
Justice Khaira <bsc-inf-28-22@unima.ac.mw>
2024-11-11 23:41:20 +0200

    modified the table structure

--- src/components/Sent message/MessageTable.tsx
+++ src/components/Sent message/MessageTable.tsx
@@ -8,7 +8,7 @@ function MessageTable() {
     { name: 'Kondwani Padyera', message: 'You have an appointment...', date: '13/11/2024' },
     { name: 'Kondwani Padyera', message: 'You have an appointment...', date: '13/11/2024' },
     { name: 'Kondwani Padyera', message: 'You have an appointment...', date: '13/11/2024' },
-     
+    { name: 'Justice    Khaira', message: 'You have an appointment...', date: '13/11/2024' },
   ];
 
   return (
