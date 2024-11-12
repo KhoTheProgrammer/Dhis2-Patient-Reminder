126cc0481410b94442bb46ebe5b3dc5616c1295f
Kondwani kachikuni <bed-com-43-20@unima.ac.mw>
2024-11-12 20:41:04 +0200

    created a better homepage

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import "./homepage.css";
 import Home from '../../assets/pictures/home.jpg' 
 
-const home = () => {
+const HomePage = () => {
   return (
     <div className="main-container">
-      <div className="title">
+      <div className="pic">
       <img src={Home} alt="" />
       </div>
     </div>
   );
 };
 
-export default home;
+export default HomePage;
