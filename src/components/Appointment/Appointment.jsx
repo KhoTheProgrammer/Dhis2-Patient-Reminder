536e67b79d90f209b4850b8ad48d1c8e32024eac
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-12 15:19:28 +0200

    Added time input to the appointment field

--- src/components/Appointment/Appointment.jsx
+++ src/components/Appointment/Appointment.jsx
@@ -3,28 +3,34 @@ import "./Appointment.css";
 
 const Appointment = ({ onClose, onConfirm }) => {
   const [selectedDate, setSelectedDate] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
 
   const handleDateChange = (event) => {
     setSelectedDate(event.target.value);
   };
 
+  const handleTimeChange = (event) => {
+    setSelectedTime(event.target.value);
+  };
+
   const handleConfirm = () => {
-    onConfirm(selectedDate); // Pass selected date to parent on confirmation
+    if (selectedDate && selectedTime) {
+      onConfirm({ date: selectedDate, time: selectedTime, id: "" });
+    } else {
+      alert("Please select both date and time.");
+    }
+    onClose();
   };
 
   return (
     <div className="appointment">
       <div>
-        <input
-          type="date"
-          value={selectedDate}
-          onChange={handleDateChange}
-          className="input"
-        />
+        <input type="date" value={selectedDate} onChange={handleDateChange} className="input"/>
+        <input type="time" value={selectedTime} onChange={handleTimeChange} />
       </div>
       <div className="buttons-container">
         <button className="buttons ok-btn" onClick={handleConfirm}>
-          OK
+          Confirm
         </button>
         <button className="buttons cancel-btn" onClick={onClose}>
           Cancel
