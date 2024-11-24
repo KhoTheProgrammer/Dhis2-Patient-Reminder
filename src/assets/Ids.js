b029c405513c1a50e34c5349ac5b8ed0ce464bf9
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-25 00:39:30 +0200

    Added all ids used in the sysdtem in one place

---
+++ src/assets/Ids.js
@@ -0,0 +1,12 @@
+export const ids = {
+  orgUnit: "rEk35avS8i1",
+  trackedEntityType: "nEenWmSyUEp",
+  firstname: "w75KJ2mc4zz",
+  lastname: "zDhUuAYrxNC",
+  dob: "cejWyOfXge6",
+  gender: "",
+  phone: "P2cwLGskgxn",
+  address: "VqEFza8wbwA",
+  programstage: "djfuWTIR5zc",
+  program: "qQIsC9hO2Gj",
+};
