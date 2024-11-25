56bd3c4c673477c579f91c222b218e3f8fbef9e6
Justice Khaira <bsc-inf-28-22@unima.ac.mw>
2024-11-25 21:31:27 +0200

    app.js error resolving

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './assets/sidebar/Sidebar';
 import NoPatientFound from './assets/NoPatientFound/NoPatientFound'
 import PatientEnrollment from './components/enrollPatient/PatientEnrollment';
-import Messages from './assets/Messages';
-import FollowUp from './assets/FollowUp';
-import Progress from './assets/Progress';
+//import Messages from './assets/Messages';
+//import FollowUp from './assets/FollowUp';
+//import Progress from './assets/Progress';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Register from './components/register/Register';
@@ -30,7 +30,7 @@ const App = () => (
           <Route path="/patients" component={NoPatientFound} />
           <Route path='/register' Component={Register} />
           <Route path="/enroll" component={PatientEnrollment} />
-          <Route path="/messages" component={Messages} />
+        
           <Route path="/follow-up" component={FollowUp} />
           <Route path="/progress" component={Progress} />
         </Routes>
