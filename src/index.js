97dce4d3c0b56fa52078f972b371b65b26fd195f
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-25 22:59:48 +0200

    added values to .env

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import dotenv from 'dotenv'
+
+dotenv.config();
+console.log("API_KEY", process.env.REACT_APP_API_KEY);
+
 
 ReactDOM.render(
   <BrowserRouter>
