a45fb03d3e87edea4411e5acfa33909e13cbba92
Justice Khaira <bsc-inf-28-22@unima.ac.mw>
2024-11-25 23:56:10 +0200

    app.js modification

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MessageTable from './components/Sent message/MessageTable';
 import HomePage from './components/Landingpage/HomePage';
 import FollowUp from './components/FollowUp/FollowUp';
 import Progress from './components/PatientProgress/Progress'
+import Patients from './components/patients/Patients';
 const App = () => (
   <Router>
     <div className="app-container">
