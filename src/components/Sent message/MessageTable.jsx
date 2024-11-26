0c71ff54857d0951015e2b0c364ce9f8a0c21f7d
MejaAdam <167316867+MejaAdam@users.noreply.github.com>
2024-11-26 13:35:13 -0800

    Saving local changes before revert

---
+++ src/components/Sent message/MessageTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MessageTable() {
+  return (
+    <div>MessageTable</div>
+  )
+}
+
+export default MessageTable
--- src/components/register/Api.jsx
+++ src/components/register/Api.jsx
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+import { ids } from "../../assets/Ids";
 
 const api = axios.create({
-  baseURL:
-    "https://data.research.dhis2.org/in5320/api", // Update with your DHIS2 instance URL
+  baseURL: "https://data.research.dhis2.org/in5320/api", // Update with your DHIS2 instance URL
   headers: {
     "Content-Type": "application/json",
     Authorization: "Basic " + btoa("admin:district"), // Replace with your credentials
@@ -11,24 +11,24 @@ const api = axios.create({
 
 // Function to register a patient in DHIS2
 export const registerPatient = async (patientData) => {
-    const data = {
-      trackedEntityType: "nEenWmSyUEp", // ID for "Person" or "Patient" entity type
-      orgUnit: "DFyu9VGpodC", // Organization unit ID
-      attributes: [
-        { attribute: "w75KJ2mc4zz", value: patientData.firstName }, // Replace with actual attribute ID
-        { attribute: "zDhUuAYrxNC", value: patientData.lastName },
-        { attribute: "iESIqZ0R0R0", value: patientData.dob },
-        { attribute: "cejWyOfXge6", value: patientData.gender },
-        { attribute: "P2cwLGskgxn", value: patientData.phone },
-        { attribute: "VqEFza8wbwA", value: patientData.address },
-      ],
-    };
+  const data = {
+    trackedEntityType: ids.trackedEntityType, // ID for "Person" or "Patient" entity type
+    orgUnit: ids.orgUnit, // Organization unit ID
+    attributes: [
+      { attribute: ids.firstname, value: patientData.firstName }, // Replace with actual attribute ID
+      { attribute: ids.lastname, value: patientData.lastName },
+      { attribute: ids.dob, value: patientData.dob },
+      { attribute: ids.gender, value: patientData.gender },
+      { attribute: ids.phone, value: patientData.phone },
+      { attribute: ids.address, value: patientData.address },
+    ],
+  };
 
-    try {
-            const response = await api.post('/trackedEntityInstances', data);
-            return response.data;
-        } catch (error) {
-            console.error('Error registering patient:', error);
-            throw error;
-                }
+  try {
+    const response = await api.post("/trackedEntityInstances", data);
+    return response.data;
+  } catch (error) {
+    console.error("Error registering patient:", error);
+    throw error;
+  }
 };
