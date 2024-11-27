a5ad6b253a913d7feb69deb3a4d4abd186b66659
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-11-27 04:13:19 +0200

    added event property

--- src/components/FollowUp/api.js
+++ src/components/FollowUp/api.js
@@ -2,12 +2,13 @@ import { ids } from "../../assets/Ids";
 const programStageId = ids.programstage;
 const password = process.env.REACT_APP_DHIS_PASSWORD;
 const username = process.env.REACT_APP_DHIS_USERNAME;
+
 export const appointmentQuery = {
   events: {
     resource: "events.json",
     params: {
       fields:
-        "trackedEntityInstance, status, enrollment, dataValues[dataElement, value]",
+        "trackedEntityInstance, status, enrollment, dataValues[dataElement, value], event",
       paging: false,
       programStage: programStageId,
       orgUnit: ids.orgUnit,
@@ -25,8 +26,28 @@ export const fetchPatientDetails = async (trackedEntityInstance) => {
     }
   );
 
-  if (!response.status === "ERROR") {
-    return false;
+  if (!response.ok) {
+    throw new Error("Failed to fetch patient details");
   }
-  return true; // Adjust according to your API response structure
+
+  return response.json(); // Adjust according to your API response structure
+};
+
+export const updateAppointmentStatus = async (appointment) => {
+  const response = await fetch(
+    `https://data.research.dhis2.org/in5320/api/events/${appointment.event}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + btoa("admin:district"),
+      },
+      body: JSON.stringify(appointment),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to update appointment status");
+  }
+  return response.json();
 };
